52672c7faabe2246777c313bdf636f7822b2d99e
dudamollica <dudarenno@yahoo.com.br>
2022-10-27 12:38:34 -0300

    Mensagens renderizadas e atualizadas

--- script.js
+++ script.js
@@ -1,20 +1,40 @@
-let listaMensagens=[]
+let listaMensagens=[];
+let mesagePromise;
+let elemento;
 
-let name=prompt("Qual o seu lindo nome?")
-
-let buscarMensagens = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages")
-
-buscarMensagens.then(verData)
-function verData(Data){ 
-    alert('oi')
-    console.log(Data)
+// let name=prompt("Qual o seu lindo nome?")
+mesagePromise = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
+mesagePromise.then(mensagensData)
+setInterval(buscarMensagens, 3000)
+function buscarMensagens(){
+mesagePromise = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
+mesagePromise.then(mensagensData)
 }
-
-randerizarMensagens()
+function mensagensData(resposta){ 
+    listaMensagens=resposta.data
+    randerizarMensagens()
+}
 
 function randerizarMensagens(){
     let chat=document.querySelector('.chat')
-    for(let i=0;i<10;i++){
-    chat.innerHTML+= `<li>oi</li>`
+    chat.innerHTML=""
+    for(let i=0;i<listaMensagens.length;i++){
+        let mensagem= listaMensagens[i];
+        // elemento=document.getElementById(`"${i}"`)
+
+    if(mensagem.text=="entra na sala..." || mensagem.text=="sai da sala..."){
+    chat.innerHTML+= `<li class="msg entrou-saiu" id=${i}> <span class="time">(${mensagem.time}) </span> 
+    <strong> ${mensagem.from}   </strong>   ${mensagem.text} </li>` 
     }
+    else if (mensagem.to=="Todos"){
+    chat.innerHTML+= `<li class="msg mensagem" ${i}> <span class="time">(${mensagem.time}) </span> 
+    <strong> ${mensagem.from}</strong>  para <strong>${mensagem.to}</strong>: ${mensagem.text} </li>`
+    } 
+    else{
+    chat.innerHTML+= `<li class="msg mensagem-reservada" id=${i}> <span class="time">(${mensagem.time}) </span> 
+    <strong> ${mensagem.from}</strong> reservadamente para <strong>${mensagem.to}</strong>: ${mensagem.text} </li>`   
+    }
+    // elemento.scrollIntoView()
+}
+ 
 }
