450de7d7d80777c465287aebd97cd3793cb6c654
dudamollica <dudarenno@yahoo.com.br>
2022-10-27 13:54:31 -0300

    Entrar na sala e manter conexão finalizado

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 let listaMensagens=[];
 let mesagePromise;
 let elemento;
+let entrouPromise;
+let nome;
 
-// let name=prompt("Qual o seu lindo nome?")
+nome=prompt("Qual o seu lindo nome?")
+entradaNaSala()
+
+//para as mensagens já aparecerem na tela assim que iniciar
 mesagePromise = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
 mesagePromise.then(mensagensData)
+//para atualizar as mensagens a cada 3 segundos
 setInterval(buscarMensagens, 3000)
 function buscarMensagens(){
 mesagePromise = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
@@ -35,6 +41,27 @@ function randerizarMensagens(){
     <strong> ${mensagem.from}</strong> reservadamente para <strong>${mensagem.to}</strong>: ${mensagem.text} </li>`   
     }
     // elemento.scrollIntoView()
+    }
 }
- 
+
+
+function entradaNaSala(){
+const entrar= {name:nome};
+entrouPromise=axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", entrar)
+entrouPromise.catch(nomeJaExiste)
+}
+
+function nomeJaExiste(){
+    nome=prompt("Já exite um usuário com este nome, por favor digite outro")
+    entradaNaSala()
+}
+
+setInterval(manterConexao, 5000)
+function manterConexao(){
+const conexaoOn= {name:nome};
+conexaoPromise=axios.post("https://mock-api.driven.com.br/api/v6/uol/status",conexaoOn)
+}
+
+function adicionarNovaMensagem(){
+
 }
